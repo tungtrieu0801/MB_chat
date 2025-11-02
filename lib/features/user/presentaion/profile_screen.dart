30627b6151400e4c39ee0ecf9deb230d18916ba1
tungtrieu0801 <trieutungvp@gmail.com>
2025-11-02 16:30:48 +0700

    feat:add profile screen

--- lib/features/user/presentaion/profile_screen.dart
+++ lib/features/user/presentaion/profile_screen.dart
@@ -7,21 +7,82 @@ class ProfileScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Trieu Tung'),
+        leading: IconButton(
+          icon: const Icon(Icons.search),
+          onPressed: () {},
+        ),
+        title: Text("Tim kiem"),
         actions: [
           IconButton(
-            icon: const Icon(Icons.search),
+            icon: const Icon(Icons.settings),
             onPressed: () {},
           ),
         ],
+        backgroundColor: Colors.grey[300],
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          CircleAvatar(
-            radius: 55.r,
-          )
-        ],
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 10.w),
+        child: Column(
+          spacing: 15.w,
+          children: [
+            SizedBox(width: 15,),
+            InkWell(
+              onTap: () {
+                print("tap");
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    child: Row(
+                      children: [
+                        CircleAvatar(
+                          radius: 30.r,
+                        ),
+                        SizedBox(width: 20.w,),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Name"),
+                            Text("Go to personal page")
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  IconButton(onPressed: () {}, icon: Icon(Icons.person))
+                ],
+              ),
+            ),
+            InkWell(
+              onTap: () {
+                print("tap");
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    child: Row(
+                      children: [
+                        Icon(Icons.style_outlined),
+                        SizedBox(width: 20.w,),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Style"),
+                            Text("Set up your style for app")
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  IconButton(onPressed: () {}, icon: Icon(Icons.person))
+                ],
+              ),
+            )
+
+          ],
+        ),
       )
     );
   }
